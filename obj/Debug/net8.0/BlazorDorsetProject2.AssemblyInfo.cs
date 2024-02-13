98a63e493d133e0bd20d4964356fe3eb4837e1d7
gabrielbaraza <gabriel.baraza@epita.fr>
2024-02-13 11:29:48 +0000

    you knpow

--- obj/Debug/net8.0/BlazorDorsetProject2.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorDorsetProject2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorDorsetProject2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+169fcc609300f3425c61c54b6bfdff7c88bce2b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorDorsetProject2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorDorsetProject2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
